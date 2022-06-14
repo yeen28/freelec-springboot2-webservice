e10132aec62aef580b5641c5088cc9b131aa38ca
yeen28 <yestar28@gmail.com>
2022-06-14 15:39:06 +0900

    [220614] 구글 로그인 연동-Role.java
    각 사용자의 권한을 관리할 Enum 클래스

---
+++ freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/domain/user/Role.java
@@ -0,0 +1,18 @@
+package com.jojoldu.book.springboot.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/*
+* 각 사용자의 권한을 관리할 Enum 클래스
+* */
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String title;
+}
